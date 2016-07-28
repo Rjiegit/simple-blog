a452d188b02f6234cb3024a3ba710ca775c7f544
jie <mjunzh@gmail.com>
2016-07-28 18:50:39 +0800

    css載入修正

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title>{{ $title }}</title>
+	<!-- 最新編譯和最佳化的 CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    <!-- 選擇性佈景主題 -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
 </head>
 <body>
     <div class="container">
@@ -16,10 +20,6 @@
     <a href="{{ URL::previous() }}" class = "btn btn-default">回上一頁</a>
     </div>
 
-    <!-- 最新編譯和最佳化的 CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-    <!-- 選擇性佈景主題 -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
     <!-- 最新編譯和最佳化的 JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </body>
